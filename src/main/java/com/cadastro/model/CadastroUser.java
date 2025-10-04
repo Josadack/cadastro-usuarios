ab9faffb4623ad92b04172d84e443282a551ed81
Josadack <josadaqueferreira@outlook.com>
2025-10-03 23:01:07 -0300

    Criação da model

---
+++ src/main/java/com/cadastro/model/CadastroUser.java
@@ -0,0 +1,65 @@
+package com.cadastro.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import org.hibernate.Length;
+
+@Entity
+@Table(name = "usuario")
+
+public class CadastroUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length=50)
+    private String nome;
+    private String foto;
+    @Column(unique = true)
+    private String email;
+    @Column(unique = true, length = 15)
+    private String telefone;
+
+    // Getters e Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
