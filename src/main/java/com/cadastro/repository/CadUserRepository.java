621cc5f75f28b9f5c16a730bca9f0a4c8cbb9815
Josadack <josadaqueferreira@outlook.com>
2025-10-06 16:24:43 -0300

    Resolução de probÃlema de merge

--- src/main/java/com/cadastro/repository/CadUserRepository.java
+++ src/main/java/com/cadastro/repository/CadUserRepository.java
@@ -5,8 +5,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 @Repository
 public interface CadUserRepository extends JpaRepository<CadastroUser, Long> {
+
     Page<CadastroUser> findByNomeContainingIgnoreCase(String nome, Pageable pageable);
+
+    @Query("SELECT u.idade AS faixa, COUNT(u) AS quantidade FROM CadastroUser u GROUP BY u.idade")
+    List<Object[]> contarUsuariosPorFaixaEtaria();
+
 }
