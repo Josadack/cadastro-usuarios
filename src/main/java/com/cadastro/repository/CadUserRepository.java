9d3c292b8b9853525d452e2d6aefdd9ac94795b7
Josadack <josadaqueferreira@outlook.com>
2025-10-06 16:21:26 -0300

    Query cutomizada para consulta

--- src/main/java/com/cadastro/repository/CadUserRepository.java
+++ src/main/java/com/cadastro/repository/CadUserRepository.java
@@ -2,6 +2,13 @@ package com.cadastro.repository;
 
 import com.cadastro.model.CadastroUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CadUserRepository extends JpaRepository<CadastroUser, Long> {
+
+
+    @Query("SELECT u.idade AS faixa, COUNT(u) AS quantidade FROM CadastroUser u GROUP BY u.idade")
+    List<Object[]> contarUsuariosPorFaixaEtaria();
 }
